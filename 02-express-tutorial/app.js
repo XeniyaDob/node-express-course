66d95b453b554a39a98e693c160ce929dfe69e96
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-03-06 14:46:19 -0500

    add all the elements of an Express application

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+const express = require("express");
+
+//invoke express
+const app = express();
+
+// setup static and middleware
+app.use(express.static("./public"));
+
+//handle page not found conditions
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(3000, () => {
+  console.log("server is listening on port 3000....");
+});
