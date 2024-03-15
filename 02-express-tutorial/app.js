64793d5ca106853ac3f6ab8b6754e7f3e5fe92e8
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-03-15 13:36:05 -0400

    Refactor - Add peopleRouter

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { people } = require("./data");
+const peopleRouter = require("./routes/people");
 
 //static assets
 app.use(express.static("./methods-public"));
@@ -10,20 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 //parse json
 app.use(express.json());
 
-app.get("/api/v1/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+app.use("/api/v1/people", peopleRouter);
+// app.get("/api/v1/people", peopleRouter);
 
-app.post("/api/v1/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide a name" });
-  }
-  people.push({ id: people.length + 1, name: name });
-  res.status(201).json({ success: true, person: name });
-});
+// app.post("/api/v1/people", peopleRouter);
+
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000....");
