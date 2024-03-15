b3de6bb8f40000984a8d5111edb73ccc1454471c
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-03-15 13:24:25 -0400

    GET and POST APIs for people

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
+const { people } = require("./data");
 
-//app.use(["/", "/about"], logger);
-app.use(logger); //apply to all urls
+//static assets
+app.use(express.static("./methods-public"));
 
-app.get("/", (req, res) => {
-  res.send("Home");
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+//parse json
+app.use(express.json());
+
+app.get("/api/v1/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", (req, res) => {
-  res.send("About");
+app.post("/api/v1/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide a name" });
+  }
+  people.push({ id: people.length + 1, name: name });
+  res.status(201).json({ success: true, person: name });
 });
 
 app.listen(3000, () => {
