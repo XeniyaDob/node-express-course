a5c44c8f99e2640e5a8b45dfb3525c3059cd5ef9
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-03-29 10:22:02 -0400

    Add select fields

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({}).select("name price");
 
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,12 +22,18 @@ const getAllProducts = async (req, res) => {
   //console.log(queryObject);
   let result = Product.find(queryObject); //Mongoose docs
 
+  //sort
   if (sort) {
-    console.log(sort);
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+
+  //select fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
 
   const products = await result;
