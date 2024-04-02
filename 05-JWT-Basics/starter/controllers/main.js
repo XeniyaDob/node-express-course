ad63f04b4d66a97426a04c182a41053db1ecbac1
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-02 14:44:24 -0400

    Add a CustomAPIError to the login controller

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,5 +1,21 @@
+const CustomAPIError = require("../errors/custom-error");
+
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+// setup authentication so only the request with JWT can access the dashboard
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup");
+  const { username, password } = req.body;
+  console.log(username, password);
+  //1 option: mongoose validation - used it in 03-task-manager -> controllers and models
+  //2 option: Joi package- we will do in later app
+
+  //3 option: check in the controller
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+  //res.send("Fake Login/Register/Signup");
 };
 
 const dashboard = async (req, res) => {
