0e0ba06ce83fc73f9964f5211cbcf241116489a9
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2024-04-02 14:15:48 -0400

    Set up basic controllers and routes

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,13 @@
+const login = async (req, res) => {
+  res.send("Fake Login/Register/Signup");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    message: `Hello, John Doe`,
+    secret: `Here is your authorized data, and your lucky number: ${luckyNumber} `,
+  });
+};
+
+module.exports = { login, dashboard };
